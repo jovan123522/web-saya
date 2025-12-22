f181d0ea3e9568df9efcb48e96c7f4cc7b7704ea
jovan123522 <punyapribadiku3@gmail.com>
2025-12-22 11:13:49 +0700

    Add files via upload

---
+++ soal3/script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    let sapaan = document.getElementById("sapaanWaktu");
+        sapaan.innerText = "PROFIL";
+    }
+function kirimPesan() {
+    let = document.getElementById("namaSaya").innerText;
+    alert(" pesan akan dikirim ke WhatsApp ");
+}
