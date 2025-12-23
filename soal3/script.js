063e34097b65fa6654d452f829d65b2aa9a40d10
jovan123522 <punyapribadiku3@gmail.com>
2025-12-23 21:57:48 +0700

    Delete soal3 directory

--- soal3/script.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    let sapaan = document.getElementById("sapaanWaktu");
-        sapaan.innerText = "PROFIL";
-    }
-function kirimPesan() {
-    let = document.getElementById("namaSaya").innerText;
-    alert(" pesan akan dikirim ke WhatsApp ");
-}
